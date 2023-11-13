9e33384485952d5750b60553decfad67effe2c8b
Paulius <paulius.michelevicius@gmail.com>
2023-11-13 19:48:54 +0000

    Update esp-rfid.ino

--- esp-rfid.ino
+++ esp-rfid.ino
@@ -1,31 +1,59 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
-#define RST_PIN 0
+#define RST_PIN 27
 #define SS_PIN 5
-#define SUCCESS 16  // relay ...
-#define ERROR_LED 2
-#define MASTER_LED 32
-#define RELAY 17
-#define SOUND 25
+#define SUCCESS 16
+#define ERROR_LED 17
+#define MASTER_LED 4
+#define RELAY 22
+#define CARD_ADDED_SOUND 21
+#define INTERNAL_LED 2
+
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+WebServer server(80);
 
-String masters[] = {}; // define master cards here 
+String masters[] = {};
 String cards[255] = {};  // for multiple cards
 int addedCards = 0;
 bool master = false;
 int MAXTICKS = 20;
+const char* ssid     = "";
+const char* password = "";
+
+void handleOpen() {
+  digitalWrite(SUCCESS, HIGH);
+  digitalWrite(RELAY, HIGH);
+  server.send(200, "text/plain", "Door have been ppened");
+}
 
 void setup() {
   Serial.begin(9600);
+  delay(10);
+  WiFi.begin(ssid, password);
   SPI.begin();
   mfrc522.PCD_Init();  // Initiate MFRC522
   pinMode(ERROR_LED, OUTPUT);
   pinMode(MASTER_LED, OUTPUT);
   pinMode(SUCCESS, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  pinMode(SOUND, OUTPUT);
+  pinMode(CARD_ADDED_SOUND, OUTPUT);
+  pinMode(INTERNAL_LED, OUTPUT);  
+
+  int retries = 10;
+
+  while (retries > 0) {
+      delay(500);
+      Serial.print(".");
+      retries--;
+  }
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/open", handleOpen);
+  server.begin();
 }
 
 String decode() {
@@ -36,20 +64,21 @@ String decode() {
     decoded.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   decoded.toUpperCase();
-  Serial.print(decoded);
+  Serial.print(decoded.substring(1));
   Serial.println();
 
   return decoded;
 }
 
 void success(bool master) {
-  digitalWrite(SUCCESS, HIGH);
   digitalWrite(RELAY, HIGH);
+  digitalWrite(SUCCESS, HIGH);
+  delay(20);
 
   if (master) {
     digitalWrite(MASTER_LED, LOW);
   } else {
-    digitalWrite(SOUND, HIGH);
+    digitalWrite(CARD_ADDED_SOUND, HIGH);
   }
   delay(3000);
 }
@@ -57,7 +86,7 @@ void success(bool master) {
 bool validateCard(String card) {
   if (addedCards == 255) {
     addedCards = 0;
-  }return false;
+  }
   for (int i = 0; i < addedCards; i++) {
     if (card.substring(1) == cards[i]) {
       digitalWrite(ERROR_LED, HIGH);
@@ -91,7 +120,6 @@ void addCard(String card) {
 }
 
 void flash() {
-      digitalWrite(SOUND, HIGH);
   delay(250);
   digitalWrite(MASTER_LED, LOW);
   delay(250);
@@ -100,13 +128,17 @@ void flash() {
 
 void loop() {
   digitalWrite(SUCCESS, LOW);
-  digitalWrite(MASTER_LED, HIGH);
-    digitalWrite(MASTER_LED, HIGH);
+  digitalWrite(MASTER_LED, LOW);
   digitalWrite(ERROR_LED, LOW);
   digitalWrite(RELAY, LOW);
-  digitalWrite(SOUND, LOW);
+  digitalWrite(CARD_ADDED_SOUND, LOW);
 
-  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
+  server.handleClient();
+
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
     return;
   }
 
@@ -141,3 +173,4 @@ void loop() {
   digitalWrite(ERROR_LED, HIGH);
   delay(1000);
 }
+
