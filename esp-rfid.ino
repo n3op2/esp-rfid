38d94af6303a2ed6c0c2478affa0cad60bfe4fca
Paulius <paulius.michelevicius@gmail.com>
2024-09-10 20:23:25 +0100

    Update esp-rfid.ino
    was pretty much in non-working state

--- esp-rfid.ino
+++ esp-rfid.ino
@@ -22,11 +22,12 @@ B9 2E D4 0D - 6
 B3 59 38 0F - edg
 */
 String masters[] = { "80 F3 83 20", "43 71 EA 10", "B3 59 38 0F", "03 40 F1 AA", "B9 2E D4 0D" };
-String cards[255] = {};  // for multiple cards
+String cards[255] = {};  // for multiple cardsa
 int addedCards = 0;
 bool master = false;
+size_t cardL = sizeof("80 F3 83 20");
 int MAXTICKS = 30;
-unsigned long prevMl = 0;
+unsigned long prev = 0;
 unsigned long interval = 30000;
 
 IPAddress ip(192, 168, 0, 10);
@@ -47,7 +48,7 @@ void initPins() {
 void connectWifi() {
   WiFi.mode(WIFI_STA);
   WiFi.config(ip, gateway, subnet, dns);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid.c_str(), password.c_str());
   int retries = 10;
 
   while (retries > 0) {
@@ -60,28 +61,26 @@ void connectWifi() {
 void handleOpen() {
   digitalWrite(SUCCESS, HIGH);
   digitalWrite(RELAY, HIGH);
-  server.send(200, "text/plain", "It's open, come on in!");
+  server.send(200, "tex\t/plain", "It's open, come on in!");
 }
 
 void setup() {
   initPins();
-  store.begin("cards", false);
+  Serial.begin(115200);
+  store.begin("rfid", false);
   addedCards = store.getInt("count", 0);
   if (addedCards > 0) {
     for (int i = 0; i < addedCards; i++) {
-      String card = store.getString(i, ""); 
-      cards[i] = card.s_str();
+      String card;
+      card = store.getString(String(i).c_str(), ""); 
+      cards[i] = card;
     }
   }
-  String ssid = "";
-  String password = ""; // store in flash mem
+    Serial.end();
+    Serial.begin(9600);
+
   store.putString("ssid", ssid); 
   store.putString("password", password);
-  /*
-  ssid = preferences.getString("ssid", ""); 
-  password = preferences.getString("password", "");
-  */
-  Serial.begin(9600);
   delay(10);
   SPI.begin();
   mfrc522.PCD_Init();  // Initiate MFRC522
@@ -104,23 +103,22 @@ String decode() {
   return decoded;
 }
 
-void success(bool master) {
+void success(bool isNew) {
+  if (isNew) {
+    digitalWrite(CARD_ADDED_SOUND, HIGH);
+    delay(1000);
+    digitalWrite(SUCCESS, HIGH);
+    return delay(1000);
+  }
+
   digitalWrite(RELAY, HIGH);
   digitalWrite(SUCCESS, HIGH);
-  delay(20);
-
-  if (master) {
-    digitalWrite(MASTER_LED, LOW);
-  } else {
-    digitalWrite(CARD_ADDED_SOUND, HIGH);
-  }
   delay(3000);
 }
 
 bool validateCard(String card) {
   if (addedCards == 255) {
-    digitalWrite(ERROR_LED, HIGH);
-    return false;
+    addedCards = 0;
   }
   for (int i = 0; i < addedCards; i++) {
     if (card.substring(1) == cards[i]) {
@@ -149,50 +147,16 @@ bool validateMaster(String card) {
 void addCard(String card) {
   if (validateCard(card)) {
     cards[addedCards] = card.substring(1);
-    store.putString(addedCards, card.substring(1));
+    store.putString(String(addedCards).c_str(), card.substring(1));
     addedCards++;
     store.putInt("count", addedCards);
-    success(false);
+    success(true);
   }
 }
 
-void flash() {
-  delay(250);
-  digitalWrite(MASTER_LED, LOW);
-  delay(250);
-  digitalWrite(MASTER_LED, HIGH);
-}
-
-void loop() {
-  unsigned long = millis();
-
-  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
-    Serial.print(millis());
-    Serial.println("Reconnecting to WiFi...");
-    WiFi.disconnect();
-    WiFi.reconnect();
-    previousMillis = currentMillis;
-  }
-  digitalWrite(SUCCESS, LOW);
-  digitalWrite(MASTER_LED, LOW);
-  digitalWrite(ERROR_LED, LOW);
-  digitalWrite(RELAY, LOW);
-  digitalWrite(CARD_ADDED_SOUND, LOW);
-
-  server.handleClient();
-
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  String card = decode();
-
+void check(String card) {
   for (int i = 0; i < 5; i++) {
     if (card.substring(1) == masters[i]) {
-      digitalWrite(MASTER_LED, HIGH);
       int ticks = 0;
 
       while (ticks <= MAXTICKS) {
@@ -201,12 +165,14 @@ void loop() {
           if (!validateMaster(newCard)) return;
           return addCard(newCard);
         }
-
-        flash();
+        digitalWrite(MASTER_LED, HIGH);
+        delay(250);
+        digitalWrite(MASTER_LED, LOW);
+        delay(250);
         ticks++;
       }
 
-      return success(true);
+      return success(false);
     }
   }
 
@@ -218,5 +184,41 @@ void loop() {
 
   digitalWrite(ERROR_LED, HIGH);
   delay(1000);
+}
+
+void hydrate() {
+  unsigned long cur = millis();
+
+  if (cur - prev >= interval) {
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    prev = cur;
+  }
+
+  digitalWrite(SUCCESS, LOW);
+  digitalWrite(MASTER_LED, LOW);
+  digitalWrite(ERROR_LED, LOW);
+  digitalWrite(RELAY, LOW);
+  digitalWrite(CARD_ADDED_SOUND, LOW);
+
+}
+
+
+void loop() {
+  hydrate();
+  server.handleClient();
+
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+
+  String card = decode();
+  
+  check(card);
 }
 
